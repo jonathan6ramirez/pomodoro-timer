86fcf7327cb288e3a342ffb0da8f3b71276f316e
Jonathan Ramirez <ramirezjonathan@yahoo.com>
2021-11-22 13:25:14 -0600

    removed the play and stop button from the main Pomodoro.js file and moved them to the play-and-stop-buttons directory. Have not yet implemented the buttons and the necessary props.

--- src/pomodoro/Pomodoro.js
+++ src/pomodoro/Pomodoro.js
@@ -246,54 +246,6 @@ function Pomodoro() {
           </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col">
-          <div
-            className="btn-group btn-group-lg mb-2"
-            role="group"
-            aria-label="Timer controls"
-          >
-            <button
-              type="button"
-              className="btn btn-primary"
-              data-testid="play-pause"
-              title="Start or pause timer"
-              onClick={() => playPause()}
-            >
-              <span
-                className={classNames({
-                  oi: true,
-                  "oi-media-play": !isTimerRunning,
-                  "oi-media-pause": isTimerRunning,
-                })}
-              />
-            </button>
-            {/* TODO: Implement stopping the current focus or break session. and disable the stop button when there is no active session */}
-            {/* TODO: Disable the stop button when there is no active session */}
-            {!session ? 
-              <button
-              type="button"
-              className="btn btn-secondary"
-              data-testid="stop"
-              title="Stop the session"
-              disabled={true}
-            >
-              <span className="oi oi-media-stop" />
-            </button> : 
-              <button
-              type="button"
-              className="btn btn-secondary"
-              data-testid="stop"
-              title="Stop the session"
-              onClick={() => handleReset()}
-            >
-              <span className="oi oi-media-stop" />
-            </button>
-            }
-            
-          </div>
-        </div>
-      </div>
       <InSession session={session} 
       focusDuration={focusDuration} 
       breakDuration={breakDuration}
